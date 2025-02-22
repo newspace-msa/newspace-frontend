c9c61b197d275e3b6a00e6614e79e3fc25025671
MinJu Chai <108432112+judymoody59@users.noreply.github.com>
2025-02-22 11:48:48 +0900

    Minju (#5)
    * feat: 로고 추가 (#1)
    
    * feat: 뉴스 페이지 사이드바 구현
    
    * feat: 관리자 공지 구현
    
    * feat: 뉴스 메인(오늘의 키워드, 기사) 구현
    
    * Yeongseo api (#4)
    
    * Minju (#2)
    
    * feat: 로고 추가 (#1)
    
    * feat: 뉴스 페이지 사이드바 구현
    
    * feat: 관리자 공지 구현
    
    * feat: 뉴스 메인(오늘의 키워드, 기사) 구현
    
    * feat: 로그인, 회원가입 UI 구현 (#3)
    
    Co-authored-by: Y0ungse <dudtj1541@naver.com>
    
    * feat: 환경변수 수정
    
    ---------
    
    Co-authored-by: Y0ungse <dudtj1541@naver.com>
    
    * feat: 뉴스 메인(카드형 스크롤 뉴스 섹션) 구현
    
    * feat: feat: 뉴스 메인에 외부 뉴스 링크 연결
    
    * feat: 개인정보수정 UI 구현
    
    src/user/editProfile.jsx 주석 확인 바랍니다!
    
    ---------
    
    Co-authored-by: Y0ungse <dudtj1541@naver.com>

--- newspace-frontend/src/App.jsx
+++ newspace-frontend/src/App.jsx
@@ -8,6 +8,8 @@ import {
 } from "react-router-dom";
 
 //Pages
+import Login from './pages/login/login.jsx'; // 로그인 페이지
+import Signup from './pages/signup/signup.jsx'; // 회원가입 페이지
 import NewsMain from './pages/news/newsMain';
 import NewsCategoryPage from './pages/news/newsCategory';
 
@@ -17,6 +19,8 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="/login" element={<Login />} />  // 로그인 페이지
+          <Route path="/signup" element={<Signup />} />  // 회원가입 페이지
           <Route path="/news/main" element={<NewsMain />} />                {/* 뉴스 메인 페이지 */}
           <Route path="/news/:category" element={<NewsCategoryPage />} />   {/* 뉴스 카테고리 페이지 */}
         </Routes>
--- newspace-frontend/src/pages/news/newsMain.jsx
+++ newspace-frontend/src/pages/news/newsMain.jsx
@@ -1,16 +1,31 @@
 import { useEffect, useRef, useState } from "react";
 
 import styled from "styled-components";
+
+import defaultProfile from "../../assets/profile.png";
+import userImg from "../../assets/user_image.png";
 import Sidebar from "./sidebar";
 import Notice from "./notice";
 import NewsKeyword from "./keywords";
 import NewsArticle from "./article";
+import UserToggle from "../user/userToggle";
+
+import {
+    NewsTrack,
+    NewsGridContainer,
+    NewsCard,
+    NewsTitle,
+    NewsImage,
+    NewsContent
+
+} from "./news_s";
+
 
 const SidebarWidth = "130px";
 
 const PageContainer = styled.div`
     display: flex;
-    min-width: 1000px; 
+    min-width: 1500px; 
 `;
 
 const SidebarContainer = styled.div`
@@ -24,13 +39,53 @@ const SidebarContainer = styled.div`
 
 const NoticeContainer = styled.div`
     position: fixed;
-    top: 15px;
+    top: 15px; 
     left: 170px;
     background-color: #ffffff; 
     z-index: 1000; 
     display: flex;
-    align-items: flex-start;
-    gap: 10px; 
+    align-items: center;
+    gap: 10px;
+`;
+
+const UserInfoContainer = styled.div`
+    position: fixed;
+    top: 15px;
+    right: 80px; 
+    display: flex;
+    align-items: center;
+    gap: 10px;
+`;
+
+const UserGreeting = styled.span`
+    font-size: 16px;
+    color: #333;
+    margin-top: 5px;
+`;
+
+const UserIconContainer = styled.img`
+    position: fixed;
+    top: 15px;
+    right: 20px; 
+    width: 40px;
+    height: 40px;
+    object-fit: cover;
+    border-radius: 50%;
+    cursor: pointer;
+`;
+
+const LoginText = styled.span`
+    font-size: 16px;
+    font-weight: bold;
+    color: #007bff;
+    cursor: pointer;
+    position: fixed;
+    top: 15px;
+    right: 20px;
+    
+    &:hover {
+        text-decoration: underline;
+    }
 `;
 
 const ContentContainer = styled.div`
@@ -59,11 +114,59 @@ const Divider = styled.div`
     width: 1320px;
     height: 2px;
     background-color: #ddd;
-    z-index: 1000;
+    z-index: 100;
 `;
 
 
 const NewsMain = () => {
+    // 로그인 상태
+    const [isAuthorized, authorize] = useState(false); 
+
+    //임시 사용자 데이터
+    const [user, setUser] = useState({
+        name: "김철수",
+        userid: "sssjj",
+        birth: "99-01-01",
+        nickname: "어피치",
+        image: userImg
+    });
+
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const logout = () => {
+        authorize(false);
+        setUser(null);
+        setDropdownOpen(false);
+    };
+
+    const dummyNews = [
+        { 
+            title: "'\"내 잔고 왜 이래?\" 주식 팔아치운 개미들…증권사 '발칵'", 
+            image: "https://img.hankyung.com/photo/202502/01.39584674.1.jpg",
+            content: "메리츠증권에서 최근 합병을 거친 한 나스닥 상장사의 주식이 일부 투자자 계좌상에 30배 많은 잔고로 표기돼 실제 매도까지 체결된 사태가 발생했다.",
+            link: "https://www.hankyung.com/article/202502211222i"
+        },
+        { 
+            title: "최악의 구직난… 사람인 취준생 55% \"합격만 하면 어디든 간다\"", 
+            image: "https://www.job-post.co.kr/news/photo/202502/127064_134591_4253.jpg", 
+            content: "구직난이 심화하면서 취업준비생들이 기업 형태를 가리지 않고 우선 취업부터 하려는 경향이 뚜렷해지고 있다.",
+            link: "https://www.job-post.co.kr/news/articleView.html?idxno=127064#rs"
+        },
+        { 
+            title: "한국, 중국의 AI 챗봇 '딥시크' 신규 앱 다운로드 제한", 
+            image: "https://ichef.bbci.co.uk/ace/ws/800/cpsprodpb/52bd/live/a41fa3c0-ecfe-11ef-a42c-d793be61fa9c.jpg.webp", 
+            content: "한국 개인정보보호위원회는 중국의 인공지능(AI) 챗봇 '딥시크(DeepSeek)'의 국내 신규 다운로드를 제한한다고 밝혔다.",
+            link: "https://www.bbc.com/korean/articles/c98387zkj7go"
+        },
+        { 
+            title: "한강, 노벨상 시상식 섰다…\“글 속의 인물들 결코 잊힐 수 없어\”", 
+            image: "https://flexible.img.hani.co.kr/flexible/normal/970/757/imgdb/original/2024/1211/4917338464636372.webp", 
+            content: "작가 한강을 비롯한 2024년 노벨상 수상자들을 위한 시상식이 10일(현지시각) 스웨덴 스톡홀름에서 열렸다.",
+            link: "https://www.hani.co.kr/arti/international/international_general/1172295.html"
+        }
+    ];
+
+    const newsList = [...dummyNews, ...dummyNews];
 
     return (
         <PageContainer>
@@ -73,12 +176,53 @@ const NewsMain = () => {
             <ContentContainer>
                 <NoticeContainer>
                 <Notice />
+                {isAuthorized ? (
+                <UserInfoContainer>
+                    <UserGreeting>
+                        안녕하세요, <strong>{user.nickname}</strong>님!
+                    </UserGreeting>
+                    <UserIconContainer 
+                        src={user.image || defaultProfile} 
+                        alt="user" 
+                        onClick={() => setDropdownOpen(!isDropdownOpen)} 
+                    />
+                    <UserToggle 
+                        isDropdownOpen={isDropdownOpen} 
+                        user={user} 
+                        profile={defaultProfile} 
+                        logout={logout} 
+                    />
+                </UserInfoContainer>
+                ) : (
+                    <LoginText onClick={() => authorize(true)}>로그인</LoginText>
+                )}
                 </NoticeContainer>
                     <NewsContainer>
                         <NewsKeyword />
-                        <NewsArticle />
+                        <a href="https://www.yna.co.kr/view/AKR20250217150300001" target="_blank" rel="noopener noreferrer">
+                            <NewsArticle style={{ cursor: "pointer" }} />
+                        </a>
                     </NewsContainer>
                     <Divider/>
+                    <NewsGridContainer>
+                    <NewsTrack>
+                    {newsList.map((news, index) => (
+                        <a 
+                            key={index} 
+                            href={news.link} 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            style={{ textDecoration: "none", color: "inherit" }} 
+                        >
+                            <NewsCard key={index}>
+                                <NewsTitle>{news.title}</NewsTitle>
+                                <NewsImage src={news.image} alt="뉴스 이미지" />
+                                <NewsContent>{news.content}</NewsContent>
+                            </NewsCard>
+                        </a>
+                        ))}
+                    </NewsTrack>
+                </NewsGridContainer>
             </ContentContainer>
         </PageContainer>
     );
