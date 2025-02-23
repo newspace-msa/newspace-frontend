97f6ea00983fabdf3a78bfaaa38032a8537e0628
judymoody59 <jmj.chai@gmail.com>
2025-02-23 21:02:06 +0900

    fix: AI 뉴스 조회 API 수정
    - 카테고리 목록을 local storage에 저장 하여 목록 유지
    
    - 상세 페이지 열람시 local storage에 저장된 데이터 활용
    
    - 카테고리 페이지 로딩 스피너 추가

---
+++ newspace-frontend/src/api/managerApi.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const BASE_URL = `${import.meta.env.VITE_NEWSPACE_TEST_BACKEND_URL}`.replace(/\/$/, '');
+
+//관리자 공지 조회
+export const fetchNotice = async (keyword) => {
+
+
+
+};
--- newspace-frontend/src/api/newsApi.jsx
+++ newspace-frontend/src/api/newsApi.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_NEWSPACE_TEST_BACKEND_URL}`.replace(/\/$/, '') + "/api/news";
+const BASE_URL = `${import.meta.env.VITE_NEWSPACE_TEST_BACKEND_URL}`.replace(/\/$/, '');
 
 // 특정 키워드에 해당하는 뉴스 목록을 가져오는 함수
 export const fetchNewsByCategory = async (keyword) => {
     try {
-        const url = `${BASE_URL}?keyword=${encodeURIComponent(keyword)}`;
+        const url = `${BASE_URL}/api/news?keyword=${encodeURIComponent(keyword)}`;
         console.log("API 요청 URL:", url); 
 
         const response = await axios.get(url);
