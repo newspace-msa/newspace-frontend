2c6b5ddf479a43774c28f48c51a5ec4f4189ad6f
judymoody59 <jmj.chai@gmail.com>
2025-02-26 02:48:42 +0900

    fix: 키워드 수정

--- newspace-frontend/src/api/keywordApi.jsx
+++ newspace-frontend/src/api/keywordApi.jsx
@@ -20,12 +20,6 @@ export const fetchKeywords = async () => {
 export const updateKeyword = async (id, name) => {
     console.log("키워드 수정 PUT 요청:", { id, name });
 
-    // name이 없거나 빈 문자열이면 에러 방지
-    if (!name.trim()) {
-        console.error("오류: 유효하지 않은 name 값입니다.");
-        throw new Error("name 값이 비어 있습니다.");
-    }
-
     try {
         const response = await axios.put(`${BASE_URL}/api/news/keyword/${id}`, { name });
 
